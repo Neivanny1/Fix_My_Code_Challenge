cb72578c7f8178c2fd68f4d29d8bb04c5d2fec85
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2023-10-20 15:32:58 +0300

    Update 2-sort.rb

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -15,12 +15,12 @@ ARGV.each do |arg|
     # insert result at the right position
     is_inserted = false
     i = 0
-    l = result.size
+    l = result.size # size of array
     while !is_inserted && i < l do
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
